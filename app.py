e852379f43e498c7b3e3ca4e2ac900fd36835476
janesharkhan0347 <janesharkhan6@gmail.com>
2026-05-23 19:23:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ def create_app(config_name=None):
     if config_name is None:
         config_name = os.environ.get('FLASK_ENV', 'default')
 
-    app = Flask(__name__)
+    app = Flask(__name__, template_folder='.')
     app.config.from_object(config[config_name])
 
     # Ensure upload folder exists
